d3430da7f8e33c7b1b4dba11ff39792b3fdc8f78
Raphael Passos <raphael251@hotmail.com>
2021-09-11 19:24:29 -0300

    test: ensure that a pre defined greeting is returned when the passed name is null

--- src/Greeter.spec.ts
+++ src/Greeter.spec.ts
@@ -1,3 +1,4 @@
+import { measureMemory } from "vm"
 import { Greeter } from "./Greeter"
 
 
@@ -6,10 +7,17 @@ function makeSUT (): { sut: Greeter } {
 }
 
 describe('greet function test', () => {
-  it('should return a simple greeting when a name is passed through the param', async () => {
+  it('should return a simple greeting when a valid name is passed', async () => {
     const { sut } = makeSUT()
     const name = 'Raphael'
     const result = sut.handle(name)
     expect(result).toBe('Hello, Raphael.')
   })
+  it('should return a pre defined greetin when the name passed is null', () => {
+    const { sut } = makeSUT()
+    const name = null
+    const result = sut.handle(name)
+    expect(result).toBe('Hello, my friend.')
+  });
+  });
 })
