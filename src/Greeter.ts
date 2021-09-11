677a7d8031c490c596d1c3b15a93ca4fac271020
Raphael Passos <raphael251@hotmail.com>
2021-09-11 19:27:15 -0300

    feat: ensure that an uppercased greeting is returned when an uppercased name is passed

--- src/Greeter.ts
+++ src/Greeter.ts
@@ -1,6 +1,7 @@
 export class Greeter {
   public handle(name: string | null): string {
     if (!name) return 'Hello, my friend.'
+    if (name === name.toUpperCase()) return `HELLO ${name}!`
     return `Hello, ${name}.`
   }
 }
