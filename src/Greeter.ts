98eb06666e35c189cbfd849e1d0abd719f9d74d6
Raphael Passos <raphael251@hotmail.com>
2021-09-11 19:52:52 -0300

    feat: ensure that Greeter class return the greeting for the two names when an array is passed

--- src/Greeter.ts
+++ src/Greeter.ts
@@ -1,7 +1,8 @@
 export class Greeter {
-  public handle(name: string | null): string {
+  public handle(name: string | null | Array<string>): string {
     if (!name) return 'Hello, my friend.'
-    if (name === name.toUpperCase()) return `HELLO ${name}!`
+    if (typeof name === 'string' && name === name.toUpperCase()) return `HELLO ${name}!`
+    if (Array.isArray(name) && name.length === 2) return `Hello, ${name[0]} and ${name[1]}.`
     return `Hello, ${name}.`
   }
 }
