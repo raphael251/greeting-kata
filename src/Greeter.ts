998fb524a1338a70fc7ed850dfecabe71e0ae7f2
Raphael Passos <raphael251@hotmail.com>
2021-09-14 14:05:04 -0300

    feat: ensure greeter class returns the correct greetings when more than 2 names is passed

--- src/Greeter.ts
+++ src/Greeter.ts
@@ -1,8 +1,26 @@
 export class Greeter {
   public handle(name: string | null | Array<string>): string {
     if (!name) return 'Hello, my friend.'
+
     if (typeof name === 'string' && name === name.toUpperCase()) return `HELLO ${name}!`
-    if (Array.isArray(name) && name.length === 2) return `Hello, ${name[0]} and ${name[1]}.`
+
+    if (Array.isArray(name)) {
+      if (name.length === 2) return `Hello, ${name[0]} and ${name[1]}.`
+
+      if (name.length > 2) {
+        let greeting = 'Hello'
+        for (let i = 0; i < name.length; i++) {
+          if (i === name.length - 1) { 
+            greeting = greeting.concat(` and ${name[i]}.`)
+            continue
+          }
+          
+          greeting = greeting.concat(`, ${name[i]}`)
+        }
+        return greeting
+      }
+    }
+
     return `Hello, ${name}.`
   }
 }
