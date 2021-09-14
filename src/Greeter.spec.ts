ceff14a3f5171c5cecdbcfaee41c117c94eae9ca
Raphael Passos <raphael251@hotmail.com>
2021-09-14 15:50:57 -0300

    test: ensure greeter class returns separated greetings for normal and uppercased names

--- src/Greeter.spec.ts
+++ src/Greeter.spec.ts
@@ -37,4 +37,10 @@ describe('greet function test', () => {
     const result = sut.handle(names)
     expect(result).toBe('Hello, Raphael, Regiane and Maria.')
   })
+  it('should return the greetings for more than two names where one or more is uppercased when an array of names is passed', () => {
+    const { sut } = makeSUT()
+    const names = ['Raphael', 'REGIANE', 'Maria']
+    const result = sut.handle(names)
+    expect(result).toBe('Hello, Raphael and Maria. AND HELLO REGIANE!')
+  })
 })
